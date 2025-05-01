eabdf7dd22a7b219c02aa14b5c7cef5c24d7f4c6
Sarthak Darade <106514870+SarthakDarade@users.noreply.github.com>
2025-05-02 01:04:32 +0530

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -7,5 +7,4 @@ const SUPABASE_URL = "https://trserver.cloud";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE";
  
 
-
-export const supabase = createClient<Database>("", "");
+export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY);
