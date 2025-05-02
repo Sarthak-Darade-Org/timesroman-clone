f931b13fbf28a80f1d490f93b308b3db3c80aee1
Sarthak Darade <106514870+SarthakDarade@users.noreply.github.com>
2025-05-02 14:14:44 +0530

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
 // Self-hosted PostgREST endpoint and JWT secret
-const supabaseUrl = "https://trserver.cloud/rest/v1/";
+const supabaseUrl = "https://192.168.31.98:8000";
 const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE";
  
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
