fe5967227e60e3ffbeef3c2d64f1c58737c33b1f
Sarthak Darade <106514870+SarthakDarade@users.noreply.github.com>
2025-05-04 00:39:58 +0530

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,10 +1,7 @@
-// Example for React
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const supabase = createClient(
-  'https://trserver.cloud/', // URL with port
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlzcyI6InN1cGFiYXNlLWRlbW8iLCJpYXQiOjE2NDE3NjkyMDAsImV4cCI6MTc5OTUzNTYwMH0.Ps0iG3Z8cNooXOGjke3zpSH7RI2C2HecfXejwa5wO1E' // From Supabase Studio > API Settings
-);
+const supabaseUrl = 'https://trserver.cloud';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlzcyI6InN1cGFiYXNlLWRlbW8iLCJpYXQiOjE2NDE3NjkyMDAsImV4cCI6MTc5OTUzNTYwMH0.Ps0iG3Z8cNooXOGjke3zpSH7RI2C2HecfXejwa5wO1E';
 
-export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
